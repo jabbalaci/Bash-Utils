44f1d2f2ae2c229e9b9c850b1ef51bfa3b133556
Jabba Laci <jabba.laci@gmail.com>
2011-04-02 21:06:33 -0400

    tocb.py added

---
+++ tocb.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+# Website: https://pythonadventures.wordpress.com/2011/03/05/copy-string-to-x-clipboards/
+# Laszlo Szathmary, 2011 (jabba.laci@gmail.com)
+#
+# Copy the text from the standard input to ALL clipboards. Thus, you can use 
+# any paste method to insert your text (middle mouse button or Shift+Insert).
+# tocb.py -> "to clipboard(s)"
+#
+# Requirement: xsel package (sudo apt-get install xsel).
+#
+# Usage: cat file.txt | tocb
+
+import sys
+import subprocess
+
+#############################################################################
+
+def text_to_clipboards(text):
+    # "primary":
+    xsel_proc = subprocess.Popen(['xsel', '-pi'], stdin=subprocess.PIPE)
+    xsel_proc.communicate(text)
+    # "clipboard":
+    xsel_proc = subprocess.Popen(['xsel', '-bi'], stdin=subprocess.PIPE)
+    xsel_proc.communicate(text)
+
+#############################################################################
+
+stuff = sys.stdin.read()
+text_to_clipboards(stuff)

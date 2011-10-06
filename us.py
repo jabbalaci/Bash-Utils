35ec0258686f4bd56f423247aa29e50b5ad858b3
Jabba Laci <jabba.laci@gmail.com>
2011-10-06 19:02:00 -0400

    example added

--- us.py
+++ us.py
@@ -4,6 +4,11 @@
 # When to use: creating directories/files and we want to avoid spaces
 #              in their names.
 #
+# Example:
+# --------
+# mv thinkpython.pdf `us "How to Think Like a Computer Scientist.pdf"`
+#     => How_to_Think_Like_a_Computer_Scientist.pdf
+#
 # Usage: us <text>
 
 import sys

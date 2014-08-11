51a4005d91d9cc733d833330d8bd879b91fe1fe1
Jabba Laci <jabba.laci@gmail.com>
2014-08-11 10:22:51 +0200

    get_public_link.py updated

--- dropbox/get_public_link.py
+++ dropbox/get_public_link.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# encoding: utf-8
 
 """
 Get the public Dropbox links of several files
@@ -24,22 +25,71 @@ If you want to copy the links to the clipboard, combine it with tocb.py:
 get_public_link <file> | tocb
 """
 
-import os
-import sys
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
 
+import hashlib
+import os
+import platform as p
+import sys
+import uuid
 from optparse import OptionParser
 
+#############################################################################
+
+def string_to_md5(content):
+    """Calculate the md5 hash of a string.
+
+    This 'string' can be the binary content of a file too."""
+    return hashlib.md5(content).hexdigest()
+
+
+def get_fingerprint(md5=False):
+    """
+    Fingerprint of the current operating system/platform.
+
+    If md5 is True, a digital fingerprint is returned.
+    """
+    sb = []
+    sb.append(p.node())
+    sb.append(p.architecture()[0])
+    sb.append(p.architecture()[1])
+    sb.append(p.machine())
+    sb.append(p.processor())
+    sb.append(p.system())
+    sb.append(str(uuid.getnode()))    # MAC address
+    text = '#'.join(sb)
+    if md5:
+        return string_to_md5(text)
+    else:
+        return text
+
+
+def get_short_fingerprint(length=6):
+    """
+    A short digital fingerprint of the current operating system/platform.
+
+    Length should be at least 6 characters.
+    """
+    assert 6 <= length <= 32
+    #
+    return get_fingerprint(md5=True)[-length:]
+
+#############################################################################
 
 # Your own Dropbox URL. No slash at the end.
 BASE_URL = 'http://dl.dropbox.com/u/144888'
-# Your own Dropbox folder in the local file system. No slash at the end.
-BASE_PATH = '/home/jabba/Dropbox/Public'
+# Your own Dropbox/Public folder in the local file system. No slash at the end.
+if get_short_fingerprint() == 'a804db':
+    BASE_PATH = '/trash2/Dropbox/Public'
+else:
+    BASE_PATH = '{home}/Dropbox/Public'.format(home=os.path.expanduser("~"))
 
 
 def verify_file(abspath):
     """Verify if the entry exists and if it's a file (not a directory)."""
     if not os.path.isfile(abspath):
-        print >>sys.stderr, "Error: I/O problem with {0}.".format(abspath)
+        print("Error: I/O problem with {0}.".format(abspath), file=sys.stderr)
         sys.exit(2)
     #
     verify_dir(os.path.split(abspath)[0])
@@ -47,8 +97,8 @@ def verify_file(abspath):
 
 def verify_dir(directory):
     """Verify if we are in the Dropbox/Public folder."""
-    if BASE_PATH not in directory:
-        print >>sys.stderr, "Error: you are not in the Dropbox/Public folder...."
+    if "/Dropbox/Public" not in directory:
+        print("Error: you are not in the Dropbox/Public folder....", file=sys.stderr)
         sys.exit(3)
 
 
@@ -61,7 +111,7 @@ def process_file(file_name):
     """Process a single file."""
     f = os.path.abspath(file_name)
     verify_file(f)
-    print get_dropbox_link(f)
+    print(get_dropbox_link(f))
 
 
 def process_curr_dir():
@@ -71,7 +121,7 @@ def process_curr_dir():
     for file_name in sorted(os.listdir(cwd)):
         if os.path.isfile(file_name):
             f = os.path.abspath(file_name)
-            print get_dropbox_link(f)
+            print(get_dropbox_link(f))
 
 
 def check_constants():

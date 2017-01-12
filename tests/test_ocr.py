c09a6a9aaa60c7403d9b117b8f86d88e7e74e13a
jabbalaci <jabba.laci@gmail.com>
2017-01-12 09:46:18 +0100

    tests cleaned up

--- tests/test_ocr.py
+++ tests/test_ocr.py
@@ -1,7 +1,3 @@
-import os
-import sys
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
-
 from pathlib import Path
 
 import config as cfg
--- tests/test_xml2json.py
+++ tests/test_xml2json.py
@@ -1,9 +1,3 @@
-# coding: utf-8
-
-import os
-import sys
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
-
 import json
 from pathlib import Path
 
